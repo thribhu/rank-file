6727dec2357886e3b9f3801783428522712f8327
thribhuvan <thribhu.kumar@gmail.com>
2018-08-04 16:53:45 +0530

    state name is not displaying in side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { HomeComponent } from './home/home.component';
+import { StateService } from '../app/state-name-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [StateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
