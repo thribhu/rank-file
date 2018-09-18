7c8688a37ebe022c43f87f751567438039c9ed96
thribhuvan <thribhu.kumar@gmail.com>
2018-09-18 14:04:41 +0530

    left-sidebar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StateService } from '../app/state-name-data.service';
 import { ConnectServerIssueService } from './shared/connect-server-issue.service';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { LeftSideBarComponent } from './left-side-bar/left-side-bar.component';
 
+const appRoutes: Routes = [
+  {path: 'sample', component: HomeComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SideBarComponent,
-    HomeComponent
+    HomeComponent,
+    MyNavComponent,
+    LeftSideBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [StateService, ConnectServerIssueService],
   bootstrap: [AppComponent]
--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { interval, Observable } from 'rxjs';
-import { StateService } from '../state-name-data.service';
+// import { StateService } from '../state-name-data.service';
 
 
 @Component({
@@ -9,12 +9,12 @@ import { StateService } from '../state-name-data.service';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-  state_name;
-  selected_state_id = '';
-  constructor(private stateService: StateService) {  }
+  // state_name;
+  // selected_state_id = '';
+  // constructor(private stateService: StateService) {  }
 
   ngOnInit() {
-  this.state_name = this.stateService.new_state;
+  // this.state_name = this.stateService.new_state;
   // console.log('state name', this.state_name);
   // this.stateService.state_name_funciton();
   }
